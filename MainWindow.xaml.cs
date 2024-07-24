bb1806f8eb2acad2b9c853a1432689fe6a8a804d
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2024-07-24 21:06:59 +0300

    The length converter has been removed. Changing the length of headers when the window is resized is done via the ListView_SizeChanged event

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using CS_WPF_Lab9_Rental_Housing.Business.Infastructure;
 using CS_WPF_Lab9_Rental_Housing.Business.Managers;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -32,6 +33,23 @@ namespace CS_WPF_Lab9_Rental_Housing
             photoManager = factory.GetPhotoManager();
 
             DbInitData.SetupData(factory);
+        }
+
+        private void ListView_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ListView? listView = sender as ListView;
+            if (listView is null) return;
+            GridView? gridView = listView.View as GridView;
+            if (gridView is null) return;
+
+            double actualWidth = listView.ActualWidth;
+            double newWidth = actualWidth/ gridView.Columns.Count;
+
+            foreach(var column in gridView.Columns)
+            {
+                column.Width = newWidth;
+            }
+            
         }
     }
 }
