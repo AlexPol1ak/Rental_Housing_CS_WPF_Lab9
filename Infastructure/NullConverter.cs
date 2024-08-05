ef35eaaf218f74e47ae0d9738189a80645407d49
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2024-08-05 21:51:54 +0300

    The handling of 0-values has been changed.  When receiving int or double 0 in the converter returns an empty string

--- Infastructure/NullConverter.cs
+++ Infastructure/NullConverter.cs
@@ -15,19 +15,15 @@ namespace CS_WPF_Lab9_Rental_Housing.Infastructure
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value == null) return string.Empty;
-            if (value != null && value is string str)
-            {
-                if( 
-                    (int.TryParse(str, out int numberInt) && numberInt ==0) || 
-                    (double.TryParse(str, out double numberDouble) && numberDouble == 0) ||
-                    (ulong.TryParse(str, out ulong numberUlong) && numberUlong == 0)
-                 ) return string.Empty;
-            }
+            if(value is int i && i==0) return string.Empty;
+            if (value is double d && d == 0) return string.Empty;
+            
             return value;
         }
 
         public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            
             if (value is string str &&
                 (string.IsNullOrEmpty(str) && string.IsNullOrWhiteSpace(str)))
                 return null;
