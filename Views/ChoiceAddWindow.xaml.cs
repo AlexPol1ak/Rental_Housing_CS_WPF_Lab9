9775e6aa51c56148db54c14c9ffd38841bd17ffd
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2024-08-05 21:56:52 +0300

    Added a selection box to create houses or apartments. The window asks the user to create houses or apartments and returns the result of the selection to ViewModel.

---
+++ Views/ChoiceAddWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using CS_WPF_Lab9_Rental_Housing.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CS_WPF_Lab9_Rental_Housing.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для ChoiceAddWindow.xaml
+    /// </summary>
+    public partial class ChoiceAddWindow : Window
+    {       
+        public ChoiceAddWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public string? Choice { get; private set; } = null;
+
+        private ICommand _buttonCommand;
+        public ICommand ButtonCommand => _buttonCommand ??= new RelayCommand
+            (
+                (obj)=>
+                {
+                    if(obj != null) Choice = (string)obj;
+                    this.DialogResult = true;
+                    this.Close();
+                }
+            );
+
+
+    }
+}
